220a2e9b3a7afb8fb2eec90c074c7f4b8e9f1e92
breakfastitem <andrew.ehrman@gmail.com>
2020-11-06 19:51:32 -0500

    Added event listener to save and maintain input fields.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 //Get date containers
 var daysContainer = $(".container");
 
+var storedVals =["","","","","","","","",""];
+/**
+ * Get local storage data
+ */
+var tempData= JSON.parse(localStorage.getItem("events"));
+
+if(tempData != null){
+    storedVals=tempData;
+}
+
+
 /**
  * Initialize date objects
  */
@@ -9,17 +20,46 @@ var timeStrings = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
 
  for(var i =0 ; i<timeStrings.length;i++){
 
-    var div = $("<div class='row'></div>");
-    var timeBox=$("<p class='col-2'></p>");
+    var form = $("<form class='row'></form>");
+
+    var timeBox=$("<p>");
+
     timeBox.text(timeStrings[i]);
-    var inputField = $("<input class='col-8'></input>");
-    var submitButton = $("<button class='col-2'></button>");
-    submitButton.attr("id","submit"+(i+1));
+    timeBox.attr("class","col-1 hour");
 
-    div.append(timeBox);
-    div.append(inputField);
-    div.append(submitButton);
+    var inputField = $("<input>");
 
-    daysContainer.append(div);
-    
+    inputField.attr("class","col-10 past");
+    inputField.attr("id","input-"+(i));
+    inputField.val(storedVals[i]);
+
+    var submitButton = $("<button>");
+
+    submitButton.attr("id","submit-"+(i));
+    submitButton.attr("class","saveBtn col-1");
+
+    form.append(timeBox);
+    form.append(inputField);
+    form.append(submitButton);
+
+    daysContainer.append(form);
+
  }
+
+ /**
+  * Add event listeners
+  */
+ $(document).ready(function(){
+    
+
+    $(".saveBtn").on("click",function(event){
+        event.preventDefault();
+
+        var idNum= event.target.id.split("-")[1];
+
+        storedVals[idNum] = $("#input-"+idNum).val();
+
+        localStorage.setItem("events",JSON.stringify(storedVals));
+     });
+
+ });
